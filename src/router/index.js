3e46219c783ab03423d615403da247eb6e7b5dd5
Sille Van Landschoot <info@sillevl.be>
2022-12-13 11:51:46 +0100

    add a help page

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
     },
+    {
+      path: "/help",
+      name: "Help page",
+      component: () => import("../views/HelpView.vue"),
+    },
   ],
 });
 
